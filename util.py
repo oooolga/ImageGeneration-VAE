e0cd20a3f1ea1a659d8dd3cba1fd1e3b5761d9d3
oooolga <olga.xu823@gmail.com>
2018-04-13 19:59:14 -0400

    fix visualization'

--- util.py
+++ util.py
@@ -1,8 +1,6 @@
 import torch
-import torch.nn.functional as F
-from vae import VariationalAutoEncoder, VariationalUpsampleEncoder, USE_CUDA
-import numpy as np
-import torch.nn.functional as F
+from vae import *
+
 import numpy as np
 import argparse, os
 import copy, scipy
@@ -11,7 +9,6 @@ import scipy.misc
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-
 
 def load_data(args):
 
@@ -49,7 +46,6 @@ def load_data(args):
     print('Finished loading data...')
     return train_loader, valid_loader, test_loader
 
-
 def get_model(mode):
     if mode == 'deconvolution':
         model = VariationalAutoEncoder()
@@ -60,89 +56,37 @@ def get_model(mode):
         model = model.cuda()
     return model
 
-
 def factorization(n):
     from math import sqrt
     for i in range(int(sqrt(float(n))), 0, -1):
         if n % i == 0:
             if i == 1: print('Who would enter a prime number of filters')
             return int(n / i), i
-
-
-def visualize_kernel(kernel_tensor, im_name='conv1_kernel.jpg', pad=1, im_scale=1.0,
-                     model_name='', rescale=True, result_path='.'):
+ 
+def visualize(tensor, im_name='conv1_kernel.jpg', pad=1, im_scale=1.0,
+              model_name='', rescale=True, result_path='.'):
 
     # map tensor wight in [0,255]
-    if rescale:
-        max_w = torch.max(kernel_tensor)
-        min_w = torch.min(kernel_tensor)
-        scale = torch.abs(max_w-min_w)
-        kernel_tensor = (kernel_tensor - min_w) / scale * 255.0
-        kernel_tensor = torch.ceil(kernel_tensor)
+    tensor *= 255
 
     # pad kernel
     p2d = (pad, pad, pad, pad)
-    padded_kernel_tensor = F.pad(kernel_tensor, p2d, 'constant', 255)
+    padded_tensor = F.pad(tensor, p2d, 'constant', 255)
 
     # get the shape of output
-    grid_Y, grid_X = factorization(kernel_tensor.size(0))
-    Y, X = padded_kernel_tensor.size(2), padded_kernel_tensor.size(3)
+    grid_Y, grid_X = factorization(tensor.size(0))
+    Y, X = padded_tensor.size(2), padded_tensor.size(3)
 
     # reshape
     # (grid_Y*grid_X) x y_dim x x_dim x num_chann
-    padded_kernel_tensor = padded_kernel_tensor.permute(0, 2, 3, 1)
-    padded_kernel_tensor = padded_kernel_tensor.cpu().view(grid_X, grid_Y*Y, X, -1)
-    padded_kernel_tensor = padded_kernel_tensor.permute(0, 2, 1, 3)
-    #padded_kernel_tensor = padded_kernel_tensor.view(1, grid_X*X, grid_Y*Y, -1)
+    padded_tensor = padded_tensor.permute(0, 2, 3, 1)
+    padded_tensor = padded_tensor.cpu().view(grid_X, grid_Y*Y, X, -1)
+    padded_tensor = padded_tensor.permute(0, 2, 1, 3)
+    #padded_tensor = padded_tensor.view(1, grid_X*X, grid_Y*Y, -1)
 
     # kernel in numpy
-    kernel_im = np.uint8((padded_kernel_tensor.data).numpy()).reshape(grid_X*X,
-                                                                       grid_Y*Y, -1)
+    kernel_im = np.uint8((padded_tensor.data).numpy()).reshape(grid_X*X,
+                                                               grid_Y*Y, -1)
     kernel_im = scipy.misc.imresize(kernel_im, im_scale, 'nearest')
     print('|\tSaving {}...'.format(os.path.join(result_path, model_name+'_'+im_name)))
     plt.imsave(os.path.join(result_path, model_name+'_'+im_name), kernel_im, origin='upper')
-
-
-def get_batch_loss(model, imgs, k=0):
-    """
-    Get a batch average loss, KL, and reconst loss
-    :param model: the model
-    :param imgs: [bsz, 3, 64, 64]
-    :param k: if 0 use pure inference else use importance weight inference
-    :return:
-        loss: [1] batch average loss. For importance weight, this is weighted average
-        kl: [1] a KL value to be displayed
-        reconst_loss [1] a reconstruction loss to be displayed
-    """
-    if k == 0:
-        kl, log_x_cond_z, lower_bound, _ = model.inference(imgs)
-        loss = -torch.mean(lower_bound)
-        kl = torch.mean(kl)
-        reconst_loss = -torch.mean(log_x_cond_z)
-    else:
-        mc_kl, mc_log_x_cond_z, lower_bounds = model.importance_inference(imgs, k)
-        kl = torch.mean(mc_kl)
-        reconst_loss = -torch.mean(mc_log_x_cond_z)
-
-        # bp_weights [bsz, k] and detach
-        bp_weights = F.softmax(lower_bounds, dim=1).detach()
-        # weighted average over lower bounds (log w)
-        weighted_lower_bounds = torch.mean(lower_bounds * bp_weights, dim=1)
-        loss = -torch.mean(weighted_lower_bounds)
-
-    return loss, kl, reconst_loss
-
-
-
-def print_all_settings(args, model):
-
-    print('Batch size:\t\t{}'.format(args.batch_size))
-    print('Total epochs:\t\t{}'.format(args.epochs))
-    print('Operation:\t\t{}'.format(args.operation))
-    print('k-sample:\t\t{}\n'.format(args.k))
-    print('Learning rate:\t\t{}\n'.format(args.lr))
-
-    num_params = 0
-    for param in model.parameters():
-        num_params += np.prod([ sh for sh in param.shape])
-    print('Model capacity:\t\t{}\n'.format(num_params))
