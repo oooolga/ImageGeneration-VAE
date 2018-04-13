b78b8facd1f5e8535d1d71e98091fc6e7e7d5781
oooolga <olga.xu823@gmail.com>
2018-04-12 21:01:51 -0400

    commit data loader

--- train.py
+++ train.py
@@ -1,26 +1,21 @@
 import argparse
-from torchvision import datasets
-from torchvision import transforms
 import torch
-import os
+import copy
+from util import *
+import pdb
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', type=str, default='/home/yuchen/School/ift6135/celeba_hw4')
+parser.add_argument('--data_path', type=str, default='./data')
 parser.add_argument('--batch_size', type=int, default=128, help='batch_size')
+parser.add_argument('--valid_prop', type=float, default=0.3, help='validation proportion')
 args = parser.parse_args()
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-])
-train_dset = datasets.ImageFolder(root=os.path.join(args.data, 'train'), transform=transform)
-train_loader = torch.utils.data.DataLoader(train_dset, batch_size=args.batch_size, shuffle=True)
+train_loader, valid_loader, test_loader = load_data(args)
 
-test_dset = datasets.ImageFolder(root=os.path.join(args.data, 'test'), transform=transform)
-test_loader = torch.utils.data.DataLoader(train_dset, batch_size=args.batch_size, shuffle=False)
 
 for img, _ in train_loader:
-    #TODO: training here
+    pdb.set_trace()
     pass
 
 for img, _ in test_loader:
---
+++ util.py
@@ -0,0 +1,36 @@
+import torch
+
+def load_data(args):
+
+    from torchvision import datasets
+    from torchvision import transforms
+    import os, copy, pdb
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+    train_dset = datasets.ImageFolder(root=os.path.join(args.data_path, 'train'),
+    								  transform=transform)
+    anchor_pt = int(len(train_dset) * (1-args.valid_prop))
+    valid_dset = copy.deepcopy(train_dset)
+
+    train_dset.imgs = train_dset.imgs[:anchor_pt]
+    valid_dset.imgs = valid_dset.imgs[anchor_pt:]
+
+    train_loader = torch.utils.data.DataLoader(train_dset,
+    										   batch_size=args.batch_size,
+    										   shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dset,
+                                               batch_size=args.batch_size,
+                                               shuffle=True)
+
+    test_dset = datasets.ImageFolder(root=os.path.join(args.data_path, 'test'),
+    								 transform=transform)
+    test_loader = torch.utils.data.DataLoader(train_dset,
+    										  batch_size=args.batch_size,
+    										  shuffle=False)
+
+    print('Finished loading data...')
+    return train_loader, valid_loader, test_loader
+ 
