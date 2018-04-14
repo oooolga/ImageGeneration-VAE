4b48e9738fa75d7e4f7c828512622a32be8a658d
Yuchen Lu <luyuchen.paul@gmail.com>
2018-04-13 23:28:17 -0400

    add script for slurm

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [[ ${PWD} != */ImageGeneration-VAE ]]
+    then
+        echo current dir is ${PWD}
+        echo Please go to project root directory!
+        exit
+fi
+
+PYTHON_BIN="/u/luyuchen/miniconda2/envs/pytorch3/bin/python" # change to yours
+${PYTHON_BIN} train.py
