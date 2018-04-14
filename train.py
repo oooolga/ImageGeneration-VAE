d6ff192c7f02aee061d2b465afb9deba8d186efc
oooolga <olga.xu823@gmail.com>
2018-04-14 00:11:31 -0400

    add save checkpoint

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import argparse
-from util import get_model, load_data, print_all_settings, get_batch_loss, visualize, get_batch_bpp
+from util import get_model, load_data, print_all_settings, get_batch_loss, visualize
 from vae import USE_CUDA
 from torch.autograd import Variable
 import torch.optim as optim
@@ -11,12 +11,13 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', type=str, default='./data')
 parser.add_argument('--result_path', type=str, default='./result')
+parser.add_argument('--model_path', type=str, default='./model')
 parser.add_argument('--model_name', type=str, default='deconvolution')
 parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--sample_size', type=int, default=36, help='sample size')
 parser.add_argument('--epochs', type=int, default=1,
                     help='total epochs')
-parser.add_argument('--print_freq', type=int, default=100,
+parser.add_argument('--print_freq', type=int, default=10,
                     help='print frequency')
 parser.add_argument('--save_freq', type=int, default=1,
                     help='save frequency')
@@ -70,14 +71,10 @@ def eval(data_loader, model, args):
 
     model.eval()
     start = time.time()
-
     total_loss = 0
-    total_bpp = 0
-
     display_loss = 0
     display_kl = 0
     display_reconst_loss = 0
-    display_bpp = 0
 
     for batch_idx, (imgs, _) in enumerate(data_loader):
         imgs = Variable(imgs).cuda() if USE_CUDA else Variable(imgs)
@@ -85,24 +82,19 @@ def eval(data_loader, model, args):
         display_loss += loss[0].data[0] / args.print_freq
         display_kl += kl[0].data[0] / args.print_freq
         display_reconst_loss += reconst_loss[0].data[0] / args.print_freq
+
         total_loss += loss[0].data[0]
 
-        bpp = get_batch_bpp(model, imgs)
-        display_bpp += bpp[0].data[0] / args.print_freq
-        total_bpp += bpp[0].data[0]
-
         if (batch_idx+1) % args.print_freq == 0:
-            print('|\t\tbatch #:{}\tloss={:.2f}\tkl={:.2f}\treconst_loss={:.2f}\tbpp={:.2f}\tuse {:.2f} sec'.format(
-                batch_idx+1, display_loss, display_kl, display_reconst_loss, display_bpp, time.time()-start))
+            print('|\t\tbatch #:{}\tloss={:.2f}\tkl={:.2f}\treconst_loss={:.2f}\tuse {:.2f} sec'.format(
+                batch_idx+1, display_loss, display_kl, display_reconst_loss, time.time()-start))
             start = time.time()
             display_loss = 0
             display_kl = 0
             display_reconst_loss = 0
-            display_bpp = 0
 
-    avg_loss = total_loss / (1+batch_idx)
-    avg_bpp = total_bpp / (1+batch_size)
-    return avg_loss, avg_bpp
+    avg_loss = total_loss/len(data_loader)
+    return avg_loss
 
 
 def sample_visualization(data_loader, model, im_name, sample_size):
@@ -129,7 +121,7 @@ def weight_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
-def interpolate_samples(data_loader, model):
+def interpolate_samples(data_loader, model, mode='train'):
     model.eval()
 
     for imgs, _ in data_loader:
@@ -140,9 +132,9 @@ def interpolate_samples(data_loader, model):
     reconst_interp_z, reconst_interp_im = model.get_interpolate_images(imgs)
     reconst_interp_z = torch.from_numpy(reconst_interp_z).cuda()
     reconst_interp_im = torch.from_numpy(reconst_interp_im).cuda()
-    visualize(reconst_interp_z, im_name='reconstruction_z.png', im_scale=1.0,
+    visualize(reconst_interp_z, im_name='reconstruction_z_{}.png'.format(mode), im_scale=1.0,
               model_name=model_name, result_path=result_path)
-    visualize(reconst_interp_im, im_name='reconstruction_im.png', im_scale=1.0,
+    visualize(reconst_interp_im, im_name='reconstruction_im_{}.png'.format(mode), im_scale=1.0,
               model_name=model_name, result_path=result_path)
 
 
@@ -154,19 +146,20 @@ if __name__ == '__main__':
     if not os.path.exists(args.result_path):
         os.makedirs(args.result_path)
 
+    if not os.path.exists(args.model_path):
+        os.makedirs(args.model_path)
+
     global batch_size, print_freq, result_path, model_name
     batch_size, print_freq, result_path = args.batch_size, args.print_freq, args.result_path
     model_name = args.model_name
 
     print_all_settings(args, model)
-    train_loader, test_loader = load_data(args)
+    train_loader, _, test_loader = load_data(args)
 
     sample_visualization(train_loader, model, 'epoch_0_train.png',
                          args.sample_size)
     sample_visualization(test_loader, model, 'epoch_0_test.png',
                          args.sample_size)
-
-    interpolate_samples(train_loader, model)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     for epoch_i in range(1, args.epochs+1):
@@ -181,6 +174,14 @@ if __name__ == '__main__':
 
         print('|\tTrain loss={}\n'.format(avg_train_loss))
         print('|\t\tEval test:')
-        avg_test_loss, avg_test_bpp = eval(test_loader, model, args)
-        print('|\tTest loss={}\tTest bpp={}\n'.format(avg_test_loss, avg_test_bpp))
+        avg_test_loss = eval(test_loader, model, args)
+        print('|\tTest loss={}\n'.format(avg_test_loss))
+
+        if epoch_i % args.save_freq == 0:
+            save_checkpoints({'epoch_i': epoch_i, 'args': args, 'state_dict': model.state_dict(),
+                               'optimizer': optimizer.state_dict()}, 
+                               os.path.join(args.model_path, model_name+str(epoch_i)+'.pt'))
+
+    interpolate_samples(train_loader, model)
+    interpolate_samples(test_loader, model, mode='test')
 
--- util.py
+++ util.py
@@ -11,35 +11,42 @@ import scipy.misc
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-from torchvision import datasets
-from transforms import Compose, TestTransform, TrainTransform
-import math
-ZERO = 1e-5 # for numeric stability
+
 
 def load_data(args):
 
-    train_transform = Compose([
-        TrainTransform(),
+    from torchvision import datasets
+    from torchvision import transforms
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
     ])
     train_dset = datasets.ImageFolder(root=os.path.join(args.data_path, 'train'),
-                                      transform=train_transform)
+                                      transform=transform)
+    anchor_pt = int(len(train_dset) * (1-args.valid_prop))
+    valid_dset = copy.deepcopy(train_dset)
+
+    train_dset.imgs = train_dset.imgs[:anchor_pt]
+    valid_dset.imgs = valid_dset.imgs[anchor_pt:]
+
     train_loader = torch.utils.data.DataLoader(train_dset,
                                                batch_size=args.batch_size,
                                                shuffle=True,
                                                drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dset,
+                                               batch_size=args.batch_size,
+                                               shuffle=True,
+                                               drop_last=True)
 
-    test_transform = Compose([
-        TestTransform(),
-    ])
     test_dset = datasets.ImageFolder(root=os.path.join(args.data_path, 'test'),
-                                     transform=test_transform)
+                                     transform=transform)
     test_loader = torch.utils.data.DataLoader(train_dset,
                                               batch_size=args.batch_size,
                                               shuffle=False,
                                               drop_last=True)
 
     print('Finished loading data...')
-    return train_loader, test_loader
+    return train_loader, valid_loader, test_loader
 
 
 def get_model(mode):
@@ -64,10 +71,10 @@ def factorization(n):
 def visualize(tensor, im_name='conv1_kernel.png', pad=1, im_scale=1.0,
               model_name='', rescale=True, result_path='.'):
 
-    # map tensor wight in [0,256]
+    # map tensor wight in [0,255]
     if rescale:
-        tensor *= 256
-        tensor = torch.floor(tensor)
+        tensor *= 255.0
+        tensor = torch.ceil(tensor)
 
     # pad kernel
     p2d = (pad, pad, pad, pad)
@@ -121,28 +128,9 @@ def get_batch_loss(model, imgs, k=0):
 
     return loss, kl, reconst_loss
 
-def _log2(x):
-    return torch.log(x) / torch.log(2)
-
-def get_batch_bpp(model, imgs):
-    """
-    :param model: the vae
-    :param imgs: [bsz, 3, 64, 64]
-    :return: batch average bpp
-    from https://arxiv.org/pdf/1705.05263.pdf sec 2.4
-    """
-    D = np.prod([sh for sh in imgs.shape[1:]] )
-
-    # [bsz, 2000]
-    lower_bounds = model.importance_inference(imgs, k=2000)[2]
-
-    # importance weighted loglikelihood
-    # [bsz]
-    LL = _log2(
-        torch.mean(torch.exp(lower_bounds), dim=1) + ZERO
-    )
-    return torch.mean(LL - D * math.log2(256))
-
+def save_checkpoints(state, model_name):
+    torch.save(state, model_name)
+    print('Finished saving model: {}'.format(model_name))
 
 def print_all_settings(args, model):
 
