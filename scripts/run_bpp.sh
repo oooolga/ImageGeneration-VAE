176522842bce191f703a01d32457d03e68899bb5
Yuchen Lu <luyuchen@elisa1.iro.umontreal.ca>
2018-04-20 14:53:46 -0400

    run bpp with 256

---
+++ scripts/run_bpp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if [[ ${PWD} != */ImageGeneration-VAE ]]
+    then
+        echo current dir is ${PWD}
+        echo Please go to project root directory!
+        exit
+fi
+
+export PYTHONUNBUFFERED=1
+PYTHON_BIN="/u/luyuchen/miniconda2/envs/pytorch3/bin/python" # change to yours
+#PYTHON_BIN="/home/luyuchen/.conda/envs/pytorch/bin/python" # change to yours
+${PYTHON_BIN} run_bpp.py --load_model ~/models/importance_nearest10.pt
