3cad0a7da735afb1a24a95c5eb9a54ba9ae44baa
Yuchen Lu <luyuchen@elisa1.iro.umontreal.ca>
2018-04-20 19:06:27 -0400

    add script running bpp

--- scripts/run_bpp.sh
+++ scripts/run_bpp.sh
@@ -9,4 +9,4 @@ fi
 export PYTHONUNBUFFERED=1
 PYTHON_BIN="/u/luyuchen/miniconda2/envs/pytorch3/bin/python" # change to yours
 #PYTHON_BIN="/home/luyuchen/.conda/envs/pytorch/bin/python" # change to yours
-${PYTHON_BIN} run_bpp.py --load_model ~/models/importance_nearest10.pt
+${PYTHON_BIN} run_bpp.py --load_model ~/models/nearest20.pt
